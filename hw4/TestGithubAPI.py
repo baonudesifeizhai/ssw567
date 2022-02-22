627db58c8da9d34fae46240838a95d4e667d4c77
baonudesifeizhai <85092850+baonudesifeizhai@users.noreply.github.com>
2022-02-21 21:52:52 -0500

    Create TestGithubAPI.py

---
+++ hw4/TestGithubAPI.py
@@ -0,0 +1,15 @@
+import unittest
+
+from githubapi import GithubApi
+
+ 
+
+class TestGithubAPI(unittest.TestCase):
+    def testGithub(self):
+        self.assertEqual(GithubApi('?'), False)
+    def testGithub2(self):
+        self.assertEqual(GithubApi('jjjpanda'), True)
+       
+if __name__ == '__main__':
+    print('Running unit tests')
+    unittest.main()

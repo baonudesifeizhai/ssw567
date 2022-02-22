a1c105f76ecb38a6be0ad41c83a31f7b79d283b0
baonudesifeizhai <85092850+baonudesifeizhai@users.noreply.github.com>
2022-02-21 22:02:42 -0500

    Update TestGithubAPI.py

--- hw4/TestGithubAPI.py
+++ hw4/TestGithubAPI.py
@@ -8,7 +8,7 @@ class TestGithubAPI(unittest.TestCase):
     def testGithub(self):
         self.assertEqual(GithubApi('?'), False)
     def testGithub2(self):
-        self.assertEqual(GithubApi('jjjpanda'), True)
+        self.assertEqual(GithubApi('baonudesifeizhai'), True)
        
 if __name__ == '__main__':
     print('Running unit tests')

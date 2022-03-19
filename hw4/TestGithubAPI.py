03dcc02fd5e5e7529888c78502b65f4a422cbdf2
baonudesifeizhai <85092850+baonudesifeizhai@users.noreply.github.com>
2022-03-19 14:30:36 -0400

    Update TestGithubAPI.py

--- hw4/TestGithubAPI.py
+++ hw4/TestGithubAPI.py
@@ -20,8 +20,29 @@ class TestGithubAPI(unittest.TestCase):
                                      "Triangle567": "8"}]
         self.assertTrue(githubapi.connect("baonudesifeizhai")[0])
 
-   
-    
+    @patch("githubapi.connect")
+    def test_repos1(self, mock_connect):
+        mock_connect.return_value = [200, {"VOLDOR": "1",
+                                     "scarlett": "13",
+                                     "scarlett": "12",
+                                     "HelloWorldRepo": "3",
+                                     "Triangle567": "8"}]
+        self.assertIs(mock_connect()[1], githubapi.connect("baonudesifeizhai")[1])
+    @patch("githubapi.connect")
+    def testConnection2(self, mock_connect):
+        mock_connect.return_value = [200, {"assimp": "6",
+                                     "Mocks": "10",
+                                     "Project1": "2",
+                                     "threads-of-life": "1"}]
+        self.assertTrue(githubapi.connect("kimkulling")[0])
+
+    @patch("githubapi.connect")
+    def test_repos2(self, mock_connect):
+        mock_connect.return_value = [200, {"assimp-net": "6",
+                                     "assimp": "10",
+                                     "Project1": "2",
+                                     "assimp_qt_viewer": "1"}]
+        self.assertIs(mock_connect()[1], githubapi.connect("kimkulling")[1])
 
 if __name__ == '__main__':
     print('Running unit tests')
